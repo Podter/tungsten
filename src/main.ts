a6d8defcaf9a000703c88e6ab31e5c0d96957c1d
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-27 15:35:51 +0700

    data dir setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { Elysia } from "elysia";
 import { env } from "./env";
+import fs from "node:fs";
+
+if (!fs.existsSync(env.DATA_DIR)) {
+  fs.mkdirSync(env.DATA_DIR);
+}
 
 new Elysia()
   .get("/", () => "Hello Elysia")
