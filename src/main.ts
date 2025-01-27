804160ca22cc4f2b9c0b228ad2e10608c95daaf2
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-27 15:38:24 +0700

    added basic api

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { Elysia } from "elysia";
 import { env } from "./env";
 import fs from "node:fs";
+import { api } from "./routes/api";
 
 if (!fs.existsSync(env.DATA_DIR)) {
   fs.mkdirSync(env.DATA_DIR);
 }
 
 new Elysia()
-  .get("/", () => "Hello Elysia")
+  .get("/", () => "Tungsten")
+  .use(api)
   .listen(env.PORT, (server) => {
     console.log(`Tungsten running at ${server.url.toString()}`);
   });
