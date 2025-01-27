fc045d58de98755f387a957004881491361bb329
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2025-01-27 15:42:17 +0700

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { Elysia } from "elysia";
 import { env } from "./env";
 import fs from "node:fs";
 import { api } from "./routes/api";
+import { swagger } from "@elysiajs/swagger";
 
 if (!fs.existsSync(env.DATA_DIR)) {
   fs.mkdirSync(env.DATA_DIR);
 }
 
 new Elysia()
+  .use(
+    swagger({
+      path: "/docs",
+    })
+  )
   .get("/", () => "Tungsten")
   .use(api)
   .listen(env.PORT, (server) => {
